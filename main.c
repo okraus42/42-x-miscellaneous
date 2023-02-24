061f104f81ac7525a3b33961ae678a9dadd88e18
Ondrej Kraus <okraus@c1r9s5.42prague.com>
2023-02-24 17:46:07 +0100

    analysing memory

--- main.c
+++ main.c
@@ -27,28 +27,24 @@ static int	ft_put_bit(const unsigned char t, int fd)
 	return (1);
 }
 
-int	ft_put_bin(const unsigned char c, int fd)
+int	ft_put_bin(void *addr, int fd)
 {
 	unsigned char	t;
+	unsigned char	c;
 	int				i;
+	int				r;
 
 	i = 8;
+	r = 0;
+	c = *(unsigned char *) addr;
 	t = c;
-	while (i != 4)
-	{
-		ft_put_bit(t, fd);
-		i--;
-		t <<= 1;
-	}
-	write (fd, " ", 1);
 	while (i)
 	{
 		ft_put_bit(t, fd);
 		i--;
 		t <<= 1;
 	}
-	write (fd, "\n", 1);
-	return (9);
+	return (r);
 }
 
 static void	ft_put_hex(void *addr, int fd)
@@ -60,7 +56,6 @@ static void	ft_put_hex(void *addr, int fd)
 	s = (unsigned char *) addr;
 	write(fd, &"0123456789abcdef"[s[i] / 16], 1);
 	write(fd, &"0123456789abcdef"[s[i] % 16], 1);
-
 }
 
 int	main(void)
@@ -82,27 +77,33 @@ int	main(void)
 	printf("b: %c | %x\n\n", b, b);
 	b = b & 'B';
 	printf("b = b & 'B';\n");
-	ft_put_bin(b, fd);
+	ft_put_bin((void *)&b, fd);
+	write (fd, "\n", 1);
 	printf("b: %c | %x\n\n", b, b);
 	b = b & 0xFF;
 	printf("b = b & 0xFF;\n");
-	ft_put_bin(b, fd);
+	ft_put_bin((void *)&b, fd);
+	write (fd, "\n", 1);
 	printf("b: %c | %x\n\n", b, b);
 	b = b | 0x20;
 	printf("b = b | 0x20;\n");
-	ft_put_bin(b, fd);
+	ft_put_bin((void *)&b, fd);
+	write (fd, "\n", 1);
 	printf("b: %c | %x\n\n", b, b);
 	b >>= 1;
 	printf("b >>= 1;\n");
-	ft_put_bin(b, fd);
+	ft_put_bin((void *)&b, fd);
+	write (fd, "\n", 1);
 	printf("b: %c | %x\n\n", b, b);
 	b <<= 2;
 	printf("b <<= 2;\n");
-	ft_put_bin(b, fd);
+	ft_put_bin((void *)&b, fd);
+	write (fd, "\n", 1);
 	printf("b: %c | %x\n\n", b, b);
 	b = ~b;
 	printf("b = ~b;\n");
-	ft_put_bin(b, fd);
+	ft_put_bin((void *)&b, fd);
+	write (fd, "\n", 1);
 	printf("b: %c | %x\n\n", b, b);
 	u = 0xA1B2C3D4;
 	x = &u;
@@ -112,17 +113,48 @@ int	main(void)
 	ft_put_hex((void *)&u + 2, 1);
 	ft_put_hex((void *)&u + 3, 1);
 	write (1, "\n", 1);
-	b = *(unsigned char *)(((void *)(&u)) + 3);
-	ft_put_bin(b, 1);
-	b = *(unsigned char *)(((void *)(&u)) + 2);
-	ft_put_bin(b, 1);
-	b = *(unsigned char *)(((void *)(&u)) + 1);
-	ft_put_bin(b, 1);
+	b = *(unsigned char *)((void *)&u + 8);
+	write (1, " ", 1);
+	ft_put_bin((void *)&b, 1);
+	b = *(unsigned char *)((void *)&u + 7);
+	write (1, " ", 1);
+	ft_put_bin((void *)&b, 1);
+	b = *(unsigned char *)((void *)&u + 6);
+	write (1, " ", 1);
+	ft_put_bin((void *)&b, 1);
+	b = *(unsigned char *)((void *)&u + 5);
+	write (1, " ", 1);
+	ft_put_bin((void *)&b, 1);
+	b = *(unsigned char *)((void *)&u + 4);
+	write (1, " ", 1);
+	ft_put_bin((void *)&b, 1);
+	b = *(unsigned char *)((void *)&u + 3);
+	write (1, " ", 1);
+	ft_put_bin((void *)&b, 1);
+	write (1, " ", 1);
+	b = *(unsigned char *)((void *)&u + 2);
+	ft_put_bin((void *)&b, 1);
+	write (1, " ", 1);
+	b = *(unsigned char *)((void *)&u + 1);
+	ft_put_bin((void *)&b, 1);
+	write (1, " ", 1);
 	b = *(unsigned char *)&u;
-	ft_put_bin(b, 1);
+	ft_put_bin((void *)&b, 1);
 	write (1, "\n", 1);
-	printf("u: %X\n\n", u);
-	c = ft_print_memory(&s[0], 1, 200);
+	printf("u: %X, &=%p\n", u, ((void *)&u + 0));
+	printf("u: %X, &=%p\n", *(unsigned int *)((void *)&u + 4), ((void *)&u + 4));
+	printf("u: %X, &=%p\n", *(unsigned int *)((void *)&u + 8), ((void *)&u + 8));
+	printf("u: %X, &=%p\n", *(unsigned int *)((void *)&u + 12), ((void *)&u + 12));
+	printf("u: %X, &=%p\n", *(unsigned int *)((void *)&u + 16), ((void *)&u + 16));
+	printf("u: %X, &=%p\n", *(unsigned int *)((void *)&u + 20), ((void *)&u + 20));
+	printf("u: %.8X, &=%p\n", *(unsigned int *)((void *)&u + 24), ((void *)&u + 24));
+	printf("u: %.8X, &=%p\n", *(unsigned int *)((void *)&u + 28), ((void *)&u + 28));
+	printf("u: %.8X, &=%p\n", *(unsigned int *)((void *)&u + 32), ((void *)&u + 32));
+	printf("u: %.8X, &=%p\n", *(unsigned int *)((void *)&u + 36), ((void *)&u + 36));
+	printf("u: %.8X, &=%p\n", *(unsigned int *)((void *)&u + 40), ((void *)&u + 40));
+	printf("u: %.8X, &=%p\n", *(unsigned int *)((void *)&u + 44), ((void *)&u + 44));
+	printf("u: %.8X, &=%p\n", *(unsigned int *)((void *)&u + 210), ((void *)&u + 210));
+	c = ft_print_memory(&s[0], 1, 400);
 	return (0);
 }
 /*
@@ -139,3 +171,4 @@ OTHER STUFF
 	s2[3000000000] = 'e';
 	c = ft_print_memory_2(&s2[0], 1, (unsigned int)4000000000);
 */
+
