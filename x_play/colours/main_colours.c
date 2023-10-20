3e244d6429ab50961adfca467b9bbb43403fc74c
Ondřej Kraus <okraus42+github@pm.me>
2023-10-20 16:45:27 +0200

    colours

--- x_play/colours/main_colours.c
+++ x_play/colours/main_colours.c
@@ -6,7 +6,7 @@
 /*   By: okraus <okraus@student.42prague.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/19 15:02:35 by okraus            #+#    #+#             */
-/*   Updated: 2023/10/19 17:41:00 by okraus           ###   ########.fr       */
+/*   Updated: 2023/10/20 16:44:25 by okraus           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,13 +20,13 @@ static unsigned int	get_colour2(int i, unsigned char r1, unsigned char g1,
 	unsigned char	g;
 	unsigned char	b;
 
-	r = ((r1) * i + (r2) * (63 - i) ) / 64;
-	g = ((g1) * i + (g2) * (63 - i) ) / 64;
-	b = ((b1) * i + (b2) * (63 - i) ) / 64;
+	r = ((r2) * i + (r1) * (64 - i) ) / 64;
+	g = ((g2) * i + (g1) * (64 - i) ) / 64;
+	b = ((b2) * i + (b1) * (64 - i) ) / 64;
 	return (r << 16 | g << 8 | b);
 }
 
-static void	print_colours2(unsigned int c1, unsigned int c2, unsigned int c[64])
+static void	print_colours2(unsigned int c1, unsigned int c2, unsigned int c[65])
 {
 	unsigned char	r1;
 	unsigned char	g1;
@@ -43,7 +43,7 @@ static void	print_colours2(unsigned int c1, unsigned int c2, unsigned int c[64])
 	g2 = c2 >> 8 & 0xFF;
 	b2 = c2 & 0xFF;
 	i = 0;
-	while (i < 64)
+	while (i < 65)
 	{
 		c[i] = get_colour2(i, r1, g1, b1, r2, g2, b2);
 		ft_printf("%^*C %C", c[i]);
@@ -59,13 +59,13 @@ static unsigned int	get_colour(int i, unsigned char r1, unsigned char g1,
 	unsigned char	g;
 	unsigned char	b;
 
-	r = (unsigned char)sqrt(((r1 * r1) * i + (r2 * r2) * (63 - i) ) / 64);
-	g = (unsigned char)sqrt(((g1 * g1) * i + (g2 * g2) * (63 - i) ) / 64);
-	b = (unsigned char)sqrt(((b1 * b1) * i + (b2 * b2) * (63 - i) ) / 64);
+	r = (unsigned char)sqrt(((r2 * r2) * i + (r1 * r1) * (64 - i) ) / 64);
+	g = (unsigned char)sqrt(((g2 * g2) * i + (g1 * g1) * (64 - i) ) / 64);
+	b = (unsigned char)sqrt(((b2 * b2) * i + (b1 * b1) * (64 - i) ) / 64);
 	return (r << 16 | g << 8 | b);
 }
 
-static void	print_colours(unsigned int c1, unsigned int c2, unsigned int c[64])
+static void	print_colours(unsigned int c1, unsigned int c2, unsigned int c[65])
 {
 	unsigned char	r1;
 	unsigned char	g1;
@@ -82,7 +82,7 @@ static void	print_colours(unsigned int c1, unsigned int c2, unsigned int c[64])
 	g2 = c2 >> 8 & 0xFF;
 	b2 = c2 & 0xFF;
 	i = 0;
-	while (i < 64)
+	while (i < 65)
 	{
 		c[i] = get_colour(i, r1, g1, b1, r2, g2, b2);
 		ft_printf("%^*C %C", c[i]);
@@ -91,12 +91,91 @@ static void	print_colours(unsigned int c1, unsigned int c2, unsigned int c[64])
 	ft_printf("\n");
 }
 
+static void	ft_fill_clog(unsigned char clog[65536])
+{
+	int	i;
+	int	a;
+	int	b;
+
+	i = 0;
+	while (i < 65536)
+	{
+		a = (i >> 8) & 0xFF;
+		b = i & 0xFF;
+		clog[i] = sqrt(((a * a) + (b * b)) / 2);
+		++i;
+	}
+}
+
+static void	ft_fill_clin(unsigned char clin[65536])
+{
+	int	i;
+	int	a;
+	int	b;
+
+	i = 0;
+	while (i < 65536)
+	{
+		a = (i >> 8) & 0xFF;
+		b = i & 0xFF;
+		clin[i] = (a + b) / 2;
+		++i;
+	}
+}
+
+static void	print_colours3(unsigned int c1, unsigned int c2,
+	unsigned char clog[65536], unsigned char clin[65536])
+{
+	unsigned char	r1;
+	unsigned char	g1;
+	unsigned char	b1;
+	unsigned char	r2;
+	unsigned char	g2;
+	unsigned char	b2;
+	int				i;
+	unsigned int	c;
+
+	r1 = c1 >> 16 & 0xFF;
+	g1 = c1 >> 8 & 0xFF;
+	b1 = c1 & 0xFF;
+	r2 = c2 >> 16 & 0xFF;
+	g2 = c2 >> 8 & 0xFF;
+	b2 = c2 & 0xFF;
+	i = 0;
+	ft_printf("%^*C  %C", c1);
+	while (i < 61)
+	{
+		c = (clog[r1 << 8 | r2] << 16)
+			| (clog[g1 << 8 | g2] << 8)
+			| (clog[b1 << 8 | b2]);
+		ft_printf("%^*C %C", c);
+		i++;
+	}
+	ft_printf("%^*C  %C\n", c2);
+	i = 0;
+	ft_printf("%^*C  %C", c1);
+	while (i < 61)
+	{
+		c = (clin[r1 << 8 | r2] << 16)
+			| (clin[g1 << 8 | g2] << 8)
+			| (clin[b1 << 8 | b2]);
+		ft_printf("%^*C %C", c);
+		i++;
+	}
+	ft_printf("%^*C  %C\n", c2);
+}
+
 int	main(int argc, char *argv[])
 {
 	unsigned int c1;
 	unsigned int c2;
-	unsigned int c[64];
+	unsigned int c[65];
+	unsigned char clog[65536];
+	unsigned char clin[65536];
 
+	ft_fill_clog(clog);
+	ft_fill_clin(clin);
+	ft_printf("%i %i \n", clog[0xFFFF], clin[0xFFFF]);
 	if (argc != 3)
 		return (ft_dprintf(2, "Incorrect number of arguments\n"));
 	else
@@ -106,5 +185,6 @@ int	main(int argc, char *argv[])
 	}
 	print_colours(c1, c2, c);
 	print_colours2(c1, c2, c);
+	print_colours3(c1, c2, clog, clin);
 	return (0);
 }
