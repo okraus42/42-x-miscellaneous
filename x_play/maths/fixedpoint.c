690ac1d72c0f0bc6f3984926bd190f6c82ae9095
Ondřej Kraus <okraus42+github@pm.me>
2023-11-09 18:09:11 +0100

    fixed number magic

--- x_play/maths/fixedpoint.c
+++ x_play/maths/fixedpoint.c
@@ -6,11 +6,12 @@
 /*   By: okraus <okraus@student.42prague.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/08 15:29:03 by okraus            #+#    #+#             */
-/*   Updated: 2023/11/08 17:22:23 by okraus           ###   ########.fr       */
+/*   Updated: 2023/11/09 18:08:59 by okraus           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
+#include <stdlib.h>
 #include "libft/header/libft.h"
 
 static char	*ft_fixstr(long long n, int declen, char* whole, char *decimal)
@@ -107,14 +108,104 @@ char	*ft_fixtoa(long long n, unsigned int fixbits)
 	return (str);
 }
 
+void	ft_round_string(char *str)
+{
+	int	last;
+	int	carry;
+
+	last = 0;
+	carry = 0;
+	while (str[last])
+		last++;
+	--last;
+	if (ft_strchr("56789", str[last]))
+		carry = 1;
+	while (carry && last && str[last - 1] != '-')
+	{
+		carry = 0;
+		--last;
+		if (str[last] == '9')
+		{
+			str[last] = '0';
+			carry = 1;
+		}
+		else if (str[last] != '.' && str[last] != '-')
+			str[last]++;
+		else if (str[last] == '.')
+			carry = 1;
+	}
+	while (str[last])
+		last++;
+	if (!carry)
+		str[--last] = '\0';
+	else
+	{
+		while (--last - 1 && str[last] != '-')
+			str[last] = str[last - 1];
+		if (str[0] != '-')
+			last--;	
+		str[last] = '1';
+	}
+}
+
+char	*ft_fix_prec(char *s, int prec)
+{
+	int		w;
+	int		d;
+	int		i;
+	char	*str;
+
+	i = 0;
+	if (!s)
+		return (NULL);
+	while (s[i] && s[i] != '.')
+		i++;
+	if (!s[i])
+		return (NULL);
+	w = i++;
+	d = 0;
+	while (s[i + d])
+		d++;
+	str = ft_calloc(w + prec + 3, sizeof(char));
+	if (!str)
+		return (NULL);
+	i = 0;
+	while (s[i] && i < w + prec + 2)
+	{
+		str[i] = s[i];
+		i++;
+	}
+	if (!s[i])
+	{
+		while (i < w + prec + 2)
+		{
+			str[i] = '0';
+			i++;
+		}
+	}
+	else
+		ft_round_string(str);
+	free(s);
+	return (str);
+}
+
 //need to add rounding for precision
 int	main(int ac, char *av[])
 {
 	char *s;
 
-	if (ac != 3)
+	if (ac != 4)
 		return (5);
-	s = ft_fixtoa(ft_latoi(av[1]), ft_atoi(av[2]));
+	//s = ft_fixtoa(ft_latoi(av[1]), ft_atoi(av[2]));
+	// +0.5 for positive - 0.5 for negative
+	// multiply by 2^bits
+	//
+	//FLOAT TO FIX (Double to fix)
+	if (atof(av[1]) >= 0)
+		s = ft_fixtoa((int)(((atof(av[1])) * 256) + 0.5), ft_atoi(av[2]));
+	else
+		s = ft_fixtoa((int)(((atof(av[1])) * 256) - 0.5), ft_atoi(av[2]));
+	s = ft_fix_prec(s, ft_atoi(av[3]));
 	ft_printf("%s\n", s);
 	free(s);
 	return (0);
